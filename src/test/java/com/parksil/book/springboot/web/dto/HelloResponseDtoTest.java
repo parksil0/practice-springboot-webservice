45178c06157f7e6cfee2620ac9f23e096d3d5e20
parksil <parksil00@naver.com>
2020-10-06 17:42:02 +0900

    20201006 oauth

--- src/test/java/com/parksil/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/parksil/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class HelloResponseDTOTest {
+public class HelloResponseDtoTest {
 
     @Test
     public void 롬복_기능_테스트() {
@@ -13,10 +13,12 @@ public class HelloResponseDTOTest {
         int amount = 1000;
 
         //when
-        HelloResponseDTO dto = new HelloResponseDTO(name, amount);
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
 
         //then
         assertThat(dto.getName()).isEqualTo(name);
         assertThat(dto.getAmount()).isEqualTo(amount);
     }
+
+
 }
