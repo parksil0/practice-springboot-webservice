38aa80bb21f508b5a22c5c052dfb1d1eb380560b
parksil <parksil00@naver.com>
2020-10-18 22:56:54 +0900

    add oauth2 login service

--- src/test/java/com/parksil/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/parksil/book/springboot/web/PostsApiControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -40,6 +41,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_등록된다() {
         //given
         String title = "title";
@@ -66,6 +68,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_수정된다() {
         //given
         Posts savedPosts = postsRepository.save(Posts.builder()
